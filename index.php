39f7dd6a4d0dde9cdfb34bab90eef6249b3d5a0f
bbvv77 <mvc@li.cm>
2020-08-17 05:17:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -22,5 +22,29 @@
             <input type="submit" value="分析"></p>
         </form>
 </div></div>
+    <?php
+date_default_timezone_set("Asia/Shanghai");
+if(isset($_COOKIE['search'])){
+    $searchs = unserialize($_COOKIE['search']);
+    print_r("<div><form action='./dx.php' method='POST'><p>继续上一次搜素<input type='hidden' type='text' name='wd' value=".$searchs[0]."> <input id='ipt' onmousemove='red(this)' onmouseout='black(this)' type='submit' value=".$searchs[0].">".date('Y/m/d/H:i',$searchs[1])."</p></form></div>");
+}
+if(isset($_COOKIE['dt'])){
+    $dt = unserialize($_COOKIE['dt']);
+    $n = $dt[1];
+    $file="./data/".$dt[0].".p";  //读出缓存 
+    if(file_exists($file)){
+        $handle=fopen($file,'r');
+        $array=unserialize(fread($handle,filesize($file)));
+        print_r("<div><form action=\"./play.php\" method='POST'><p>继续上一次观看");
+        print_r("<input type=\"hidden\" name=\"wd\" value=".$dt[0].">");
+        print_r("<input type=\"hidden\" name=\"id\" value=".$n.">");
+        if(isset($_COOKIE['jishu'])){
+            print_r("<input type=\"hidden\" name=\"js\" value=".$_COOKIE['jishu'].">");
+        }
+        print_r("<input id='ipt' onmousemove='red(this)' onmouseout='black(this)' type=\"submit\" value=".$array[$n]['title'].@$array[$n]["tag"][$_COOKIE['jishu']]."></p></form></div>");
+        
+    }
+}
+?>
 </body>
 </html>
